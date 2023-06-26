f23ab25c57309294724bba34c2fb4a809c025f17
Riabov Vladyslav <project5@sviteco.com>
2023-06-26 15:26:26 +0300

    feat: add list groups and set up zooming

--- src/App.js
+++ src/App.js
@@ -1,10 +1,152 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import Map from "./components/Map/Map";
-
+import Grid from "@mui/material/Grid";
+import { ToggleButtonGroup, ToggleButton } from "@mui/material";
 function App() {
+  const [data, setData] = useState({
+    level1: {
+      geojsonUrl: "https://sc-test-data-uk.netlify.app/great_britain_1.geojson",
+      valuesUrl:
+        "https://sc-test-data-uk.netlify.app/data_great_britain_1.json",
+    },
+    level2: {
+      geojsonUrl: "https://sc-test-data-uk.netlify.app/great_britain_2.geojson",
+      valuesUrl:
+        "https://sc-test-data-uk.netlify.app/data_great_britain_2.json",
+    },
+  });
+  const [selectedFeatureId, setSelectedFeatureId] = useState(null);
+  const [level, setLevel] = useState("level1");
+  useEffect(() => {
+    setSelectedFeatureId(null);
+  }, [level]);
+  const handleLevelChange = (event, newLevel) => {
+    if (!newLevel) return;
+    setSelectedFeatureId(null);
+    setLevel(newLevel);
+  };
+
+  const fetchData = async () => {
+    try {
+      const requests = Object.values(data).map((lvl) =>
+        Promise.all([
+          fetch(lvl.geojsonUrl).then((response) => response.json()),
+          fetch(lvl.valuesUrl).then((response) => response.json()),
+        ])
+      );
+      const responses = await Promise.all(requests);
+      const updatedData = Object.keys(data).reduce((result, level, index) => {
+        const levelName = `lvl${level.match(/\d+/)[0]}_name`;
+        const geoData = responses[index][0];
+        const geoValues = responses[index][1];
+        const CombineData = geoData.features.map((feature) => {
+          const item = geoValues.features.find((i) => {
+            return i.properties[levelName] === feature.properties[levelName];
+          });
+          return {
+            ...feature,
+            name: item.properties[levelName],
+            density: item.properties["Density"],
+          };
+        });
+        result[level] = {
+          data: CombineData,
+        };
+        return result;
+      }, {});
+      setData(updatedData);
+      console.log(updatedData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleButtonClick = (event, featureId) => {
+    if (!featureId) return;
+    console.log(featureId);
+
+    setSelectedFeatureId(featureId);
+  };
+  const ButtonStyle = {
+    height: "50px",
+    width: "200px",
+  };
   return (
     <div className="App">
-      <Map />
+      <Grid container direction="row" sx={{ height: "100%", width: "100%" }}>
+        <Grid
+          container
+          item
+          direction="column"
+          justifyContent="center"
+          alignItems="stretch"
+          xs={4}
+        >
+          <Grid
+            item
+            xs={3}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <h2>Level of Administrative Divisions</h2>
+            <ToggleButtonGroup
+              color="primary"
+              orientation="vertical"
+              value={level}
+              exclusive
+              onChange={handleLevelChange}
+            >
+              <ToggleButton value="level1" style={ButtonStyle}>
+                Level 1
+              </ToggleButton>
+              <ToggleButton value="level2" style={ButtonStyle}>
+                Level 2
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Grid>
+          <Grid item xs={9}>
+            <h2>Administrative Divisions</h2>
+            <ToggleButtonGroup
+              color="primary"
+              orientation="vertical"
+              value={selectedFeatureId}
+              exclusive
+              onChange={handleButtonClick}
+              style={{ maxHeight: "600px", overflow: "auto" }}
+            >
+              {data[level].data &&
+                data[level].data.map((feature) => (
+                  <ToggleButton
+                    key={feature.id}
+                    value={feature.id}
+                    style={ButtonStyle}
+                  >
+                    {feature.name}
+                  </ToggleButton>
+                ))}
+            </ToggleButtonGroup>
+          </Grid>
+        </Grid>
+        <Grid item xs={8}>
+          {data[level].data && (
+            <Map
+              geoData={data[level].data}
+              selectedFeatureId={selectedFeatureId}
+              setSelectedFeatureId={setSelectedFeatureId}
+              level={level}
+              setLevel={setLevel}
+            />
+          )}
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,33 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import style from "./Map.module.css";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
-export default function Map() {
-  const [geoJson, setGeoJson] = useState(null);
-  const [values, setValuses] = useState(null);
-  const [levelName, setLevelName] = useState("lvl1_name");
-  const fetchData = async (url, setValue) => {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error("Sorry something went wrong");
+import { MapContainer, TileLayer, GeoJSON, useMap } from "react-leaflet";
+export default function Map({
+  geoData,
+  level,
+  setLevel,
+  selectedFeatureId,
+  setSelectedFeatureId,
+}) {
+  const maxZoom = level === "level1" ? 7 : null;
+  const minZoom = level === "level2" ? 8 : 1;
+  const ZoomLevelIndicator = () => {
+    const map = useMap();
+    map.on("zoomend", () => {
+      const newLevel = map.getZoom() >= 8 ? "level2" : "level1";
+      console.log(newLevel, level);
+      if (newLevel !== level) {
+        setLevel((prev) => {
+          if (prev === level && level !== newLevel) {
+            setSelectedFeatureId(null);
+          }
+          return newLevel;
+        });
+        console.log("fired");
       }
-      const data = await response.json();
-      setValue(data);
-    } catch (error) {
-      console.log(error);
-    }
+    });
+
+    useEffect(() => {
+      if (selectedFeatureId) {
+        const { bbox } = geoData.find((item) => item.id === selectedFeatureId);
+        const bounds = [
+          [bbox[3], bbox[2]],
+          [bbox[1], bbox[0]],
+        ];
+        map.fitBounds(bounds, { maxZoom: maxZoom, minZoom: minZoom });
+      }
+    }, [selectedFeatureId]);
+
+    return null;
   };
-  useEffect(() => {
-    fetchData(
-      "https://sc-test-data-uk.netlify.app/great_britain_1.geojson",
-      setGeoJson
-    );
-    fetchData(
-      "https://sc-test-data-uk.netlify.app/data_great_britain_1.json",
-      setValuses
-    );
-  }, []);
+
   function getColor(d) {
     return d > 100
       ? "#FC4E2A"
@@ -39,13 +52,21 @@ export default function Map() {
       ? "#FED976"
       : "#FFEDA0";
   }
+  const handleClick = (event) => {
+    const layer = event.target;
+    setSelectedFeatureId(layer.feature.id);
+    const bounds = layer.getBounds();
+    if (bounds.isValid()) {
+      const map = layer._map; // Access the underlying Leaflet map instance
+      map.fitBounds(bounds, { maxZoom: maxZoom, minZoom: minZoom });
+    }
+  };
   function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>hello</h3>");
     layer.on({
       mouseover: (e) => {
         e.target.setStyle({
-          // dashArray: "1",
-          fillOpacity: 0.3,
+          fillOpacity: 0.1,
         });
       },
       mouseout: (e) => {
@@ -54,14 +75,17 @@ export default function Map() {
           fillOpacity: 0.7,
         });
       },
+      click: (e) => handleClick(e),
     });
   }
+
   return (
     <>
-      {geoJson && values && (
+      {geoData && (
         <MapContainer
           className={style.containter}
           zoom={6}
+          minZoom={1}
           center={[53.89875435070986, -3.8841696195669964]}
         >
           <TileLayer
@@ -69,26 +93,25 @@ export default function Map() {
             attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
           />
           <GeoJSON
-            data={geoJson}
+            key={level}
+            data={geoData}
             style={(location) => {
-              let item = values.features.find((i) => {
-                return (
-                  i.properties[levelName] === location.properties[levelName]
-                );
-              });
-              let color = getColor(item.properties["Density"]);
+              let fillColor =
+                selectedFeatureId === location.id
+                  ? "red"
+                  : getColor(location.density);
               let result = {
-                fillColor: color,
+                fillColor: fillColor,
                 weight: 2,
                 opacity: 1,
                 color: "white",
-                // dashArray: "8",
                 fillOpacity: 0.7,
               };
               return result;
             }}
             onEachFeature={onEachFeature}
           />
+          <ZoomLevelIndicator />
         </MapContainer>
       )}
     </>
